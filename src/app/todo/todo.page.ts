85803f1192cfdbebf06dd4af8b6c7635e55bc36b
Akhil Johnson <akhiljohnson2000@gmail.com>
2021-06-11 02:23:08 +0530

    todo component

--- src/app/todo/todo.page.ts
+++ src/app/todo/todo.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonReorderGroup } from '@ionic/angular';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['todo.page.scss']
 })
 export class TodoPage {
+  items = [1, 2, 3, 4, 5];
+
 
   constructor() {}
+
+  doReorder(ev) {
+    // Before complete is called with the items they will remain in the
+    // order before the drag
+    console.log('Before complete', this.items);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. Update the items variable to the
+    // new order of items
+    this.items = ev.detail.complete(this.items);
+
+    // After complete is called the items will be in the new order
+    console.log('After complete', this.items);
+  }
 
 }
